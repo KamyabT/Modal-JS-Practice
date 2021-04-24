bc891d9bff222c9532467bb55a02876d398d099b
KamyabT <dgghost034@gmail.com>
2021-04-24 22:51:40 +0430

    Modal Practice

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+const openBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal-overlay');
+
+
+
+openBtn.addEventListener('click', function(){
+    modal.classList.add('open-modal')
+});
+
+
+
+closeBtn.addEventListener('click', function(){
+    modal.classList.remove('open-modal')
+})
